228f2432ae2af8f098d16aba6613ab203f2339a2
giovanni <giovanni_ge@hotmai.com>
2023-08-15 22:04:30 -0300

    feat: adicionando novo codigo

--- 15-08-2023/script.js
+++ 15-08-2023/script.js
@@ -1,18 +1,22 @@
+/*Criando variaveis */
 const nome = document.querySelector("#nome");
-const telefone = document.querySelector("telefone");
 const button = document.querySelector("button");
 const lista = document.querySelector(".lista");
 
+/*Eventos de javascript */
 button.addEventListener("click", (event) => {
   event.preventDefault();
   const inputValue = nome.value;
   const templateHTML = `<li>${inputValue}</li>`;
 
+  /*Condição */
   if (inputValue === "") {
-    alert("digite alguma coisa");
+    alert("Digite alguma coisa");
     return;
   }
 
+  /*Incluindo intens no HTML */
   lista.innerHTML += templateHTML;
+  /*Limpando meu campo */
   nome.value = "";
 });
