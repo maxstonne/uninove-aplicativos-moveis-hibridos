c837be93c98e6a91973757bb517ffaf7c5523a39
giovanni <giovanni_ge@hotmai.com>
2023-08-22 21:55:32 -0300

    feat: adicionando projeto

---
+++ 22-08-2023/aula-03-new/src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+
+  return (
+    <>
+     <div>
+        <h1>Olá Mundo!</h1>
+        <strong>Testando</strong>
+        <marquee>teste</marquee>
+        <hr />
+        <ul>
+          <li>Giovanni</li>
+          <li>Caio</li>
+        </ul>
+     </div>
+    </>
+  )
+}
+
+export default App
