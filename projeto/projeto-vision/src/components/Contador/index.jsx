4ca3e0023b7fc5e537d75513e9db2c8a82666295
giovanni <giovanni_ge@hotmai.com>
2023-09-05 21:53:15 -0300

    feat: adicionando arquivos

--- projeto/projeto-vision/src/components/Contador/index.jsx
+++ projeto/projeto-vision/src/components/Contador/index.jsx
@@ -1,7 +1,18 @@
+import "./style.css";
+
 export function Contador() {
   return (
     <div>
       <h1>Contador</h1>
+
+      <div className="buttons">
+        <button>Increment</button>
+        <button>Decrement</button>
+      </div>
+
+      <div className="result">
+        <strong>Vai ser o resultado</strong>
+      </div>
     </div>
   )
 }
